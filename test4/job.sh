3c12faec1cff9189bd65d7a08fb4e0c8dbce819b
fontaine618 <simfont@umich.edu>
2023-02-17 00:46:42 -0500

    added more time

--- test4/job.sh
+++ test4/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=bci_test4
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=0:10:00
+#SBATCH --time=50:00:00
 #SBATCH --array=0-2
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
