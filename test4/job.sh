1d2937166c43a85ce7b72d868c34cc8c45ad5c3b
fontaine618 <simfont@umich.edu>
2023-02-16 23:47:42 -0500

    test4 jobs update

--- test4/job.sh
+++ test4/job.sh
@@ -5,13 +5,15 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=0:10:00
+#SBATCH --array=0-2
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=20g
-#SBATCH --output=/home/%u/%x-%j.log
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=20g
+#SBATCH --output=/home/%u/logs/%x-%j.log
 # The application(s) to execute along with its input arguments and options:
 module load python/3.10.4
 source /home/simfont/venvs/bci/bin/activate
