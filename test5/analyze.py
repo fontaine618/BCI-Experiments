04d0cecc9614e9ca0eb88672259e76dc6b1259c7
fontaine618 <simfont@umich.edu>
2023-03-05 22:22:59 -0500

    plots for test5

--- test5/analyze.py
+++ test5/analyze.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 plt.style.use("seaborn-v0_8-whitegrid")
 
 
 # =============================================================================
 # SETUP
+method = "posterior_mean"
 dir = "/home/simon/Documents/BCI/experiments/test5/"
 dir_figures = dir + "figures/"
-dir_results = dir + "results/posterior_mean/"
+dir_results = dir + f"results/{method}/"
 # -----------------------------------------------------------------------------
 
 
@@ -30,13 +33,18 @@ for n in range(1, 16):
 
 # =============================================================================
 # PLOT RESULTS
-fig, axs = plt.subplots(2, 2, figsize=(12, 9), sharex="all", sharey="all")
+cmap = LinearSegmentedColormap.from_list("RdYlGn", [(0, "#d73027"), (0.9, "#ffffbf"), (1, "#1a9850")])
+
+fig, axs = plt.subplots(2, 2, figsize=(12, 8), sharex="all", sharey="all")
 for row, predset in enumerate(["train", "test"]):
-    df = results[results["pred_set"] == predset]
+    df = results.loc[results["pred_set"] == predset]
     for col, metric in enumerate(["hamming_distance", "accuracy"]):
-        mat = pd.pivot(df, index="training_reps",
-                       columns="testing_reps", values=metric).values
-        axs[row, col].imshow(mat, cmap="viridis")
+        mat = pd.pivot(df, index="training_reps", columns="testing_reps", values=metric).values
+        print(mat)
+        if metric == "hamming_distance":
+            axs[row, col].imshow(mat, cmap=cmap.reversed())
+        else:
+            axs[row, col].imshow(mat, cmap=cmap)
         axs[row, col].invert_yaxis()
         axs[row, col].set_xticks(range(0, 15))
         axs[row, col].set_yticks(range(0, 15))
@@ -49,6 +57,7 @@ for row, predset in enumerate(["train", "test"]):
             axs[row, col].set_xlabel("Testing repetitions")
         axs[row, col].set_title(predset.capitalize())
     axs[row, 0].set_ylabel("Training repetitions")
+    axs[row, 0].set_ylim(1.5, 14.5)
 plt.tight_layout()
-fig.savefig(dir_figures + "prediction_posterior_mean.pdf")
+fig.savefig(dir_figures + f"prediction_{method}.pdf")
 # -----------------------------------------------------------------------------
