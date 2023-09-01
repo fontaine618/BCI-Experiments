f58ce03e8a866fd083900b3fa2ac1af903dbac01
fontaine618 <simfont@umich.edu>
2023-09-01 10:55:58 -0400

    all jobs long

--- tuning/job.sh
+++ tuning/job.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=bci_tuning
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=1:00:00
-#SBATCH --array=0-0
+#SBATCH --time=10:00:00
+#SBATCH --array=0-35
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
