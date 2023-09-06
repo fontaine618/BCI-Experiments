4eb4e75ca5eb873a04174d235f9aed2fcccedde9
fontaine618 <simfont@umich.edu>
2023-09-06 00:43:39 -0400

    predict job for tuning experiment

---
+++ tuning/job_predict.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=bci_tuning_predict
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=2:00:00
+#SBATCH --array=0-35
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=16g
+#SBATCH --output=/home/%u/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+module load python/3.10.4
+source /home/simfont/venvs/bci/bin/activate
+python -O predict.py $SLURM_ARRAY_TASK_ID
