c053d8dfe90f84b4fe44ab980c5fcc714a8970b8
fontaine618 <simfont@umich.edu>
2023-09-19 21:33:40 -0400

    additional tuning experiments

---
+++ tuning/heterogeneity.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=xi_var
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=10:00:00
+#SBATCH --array=0-6
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=16g
+#SBATCH --output=/home/%u/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+module load python/3.10.4
+source /home/simfont/venvs/bci/bin/activate
+python -O xi_var.py $SLURM_ARRAY_TASK_ID
