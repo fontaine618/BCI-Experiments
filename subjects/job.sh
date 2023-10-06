06c83e658100762d6d44d19dfcdbbd5bd29a0eba
fontaine618 <simfont@umich.edu>
2023-10-05 22:57:40 -0400

    rerun predict and swlda

--- subjects/job.sh
+++ subjects/job.sh
@@ -18,7 +18,7 @@
 module load python/3.10.4
 source /home/simfont/venvs/bci/bin/activate
 #python -O train.py $SLURM_ARRAY_TASK_ID
-#python -O predict_loo.py $SLURM_ARRAY_TASK_ID
-#python -O predict_frt.py $SLURM_ARRAY_TASK_ID
+python -O predict_loo.py $SLURM_ARRAY_TASK_ID
+python -O predict_frt.py $SLURM_ARRAY_TASK_ID
 python -O swlda.py $SLURM_ARRAY_TASK_ID
 
