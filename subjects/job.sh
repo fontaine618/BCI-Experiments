678337fc0be4fa49bb44b0b23fac7cedc3411242
fontaine618 <simfont@umich.edu>
2023-10-05 22:57:49 -0400

    rerun predict and swlda

--- subjects/job.sh
+++ subjects/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=bci_subject
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --array=0
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
