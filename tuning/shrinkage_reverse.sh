f3b92727e507611e8d0a3bac8c99fdff65012264
fontaine618 <simfont@umich.edu>
2023-10-11 23:36:59 -0400

    experiment with reverse initialization

---
+++ tuning/shrinkage_reverse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=shrinkage_reverse
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=8:00:00
+#SBATCH --array=0-5
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=16g
+#SBATCH --output=/home/%u/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+module load python/3.10.4
+source /home/simfont/venvs/bci/bin/activate
+python -O shrinkage_reverse.py $SLURM_ARRAY_TASK_ID
