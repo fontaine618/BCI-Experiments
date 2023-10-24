7b0ce3b44e65548ea728f215651df5e15936d41a
fontaine618 <simfont@umich.edu>
2023-10-24 16:54:36 -0400

    full chain training

--- models/job.sh
+++ models/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=bci_models
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=0:20:00
+#SBATCH --time=8:00:00
 #SBATCH --array=0-4
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
