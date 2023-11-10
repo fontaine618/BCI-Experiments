224c6829c979d50a3aede5382b0f42ac963d9910
fontaine618 <simfont@umich.edu>
2023-11-10 16:58:20 -0500

    sim K experiment frist

--- sim_K/job.sh
+++ sim_K/job.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=bci_sim_K
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=1:00:00
-#SBATCH --array=0
+#SBATCH --time=5:00:00
+#SBATCH --array=0-26
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
