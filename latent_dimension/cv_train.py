005d7e619cb1ffaf5b38221c7f99cfcb9de1cfd9
fontaine618 <simfont@umich.edu>
2023-11-15 22:14:47 -0500

    added CV

---
+++ latent_dimension/cv_train.py
@@ -0,0 +1,153 @@
+import sys
+import os
+import torch
+import time
+import pickle
+import itertools as it
+sys.path.insert(1, '/home/simfont/Documents/BCI/src')
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
+from source.data.k_protocol import KProtocol
+from source.bffmbci.bffm import BFFModel
+
+# =============================================================================
+# SETUP
+dir_data = "/home/simfont/Documents/BCI/K_Protocol/"
+dir_chains = "/home/simfont/Documents/BCI/experiments/latent_dimension/chains/"
+os.makedirs(dir_chains, exist_ok=True)
+
+# file
+type = "TRN"
+subject = "114"
+session = "001"
+name = f"K{subject}_{session}_BCI_{type}"
+filename = dir_data + name + ".mat"
+
+# preprocessing
+window = 800.0
+bandpass_window = (0.1, 15.0)
+bandpass_order = 2
+downsample = 8
+
+# experiments
+nfolds = 3
+Ks = list(range(2, 14))
+folds = list(range(nfolds))
+
+# combinations
+combinations = it.product(folds, Ks)
+
+# current experiment from command line
+i = int(sys.argv[1])
+fold, K = list(combinations)[i]
+
+# model
+seed = 0
+n_iter = 20_000
+cor = 0.8
+shrinkage = 3.
+heterogeneity = 3.
+xi_var = 0.1
+sparse = False
+# -----------------------------------------------------------------------------
+
+
+# =============================================================================
+# LOAD DATA
+eeg = KProtocol(
+    filename=filename,
+    type=type,
+    subject=subject,
+    session=session,
+    window=window,
+    bandpass_window=bandpass_window,
+    bandpass_order=bandpass_order,
+    downsample=downsample,
+)
+# Select training fold
+nreps = eeg.stimulus_data["repetition"].max()
+foldid = torch.floor(torch.arange(nreps) * nfolds / (nreps-1)).clamp(0, nfolds-1)
+trnfold = foldid != fold
+trnfold = torch.arange(1, nreps+1)[trnfold]
+tstfold = foldid == fold
+tstfold = torch.arange(1, nreps+1)[tstfold]
+eeg = eeg.repetitions(trnfold.cpu().numpy().tolist())
+# -----------------------------------------------------------------------------
+
+
+# =============================================================================
+# INITIALIZE MODEL
+settings = {
+    "latent_dim": K,
+    "n_channels": eeg.sequence.shape[1],
+    "stimulus_to_stimulus_interval": eeg.stimulus_to_stimulus_interval,
+    "stimulus_window": eeg.stimulus_window,
+    "n_stimulus": (12, 2),
+    "n_sequences": eeg.sequence.shape[0],
+    "nonnegative_smgp": False,
+    "scaling_activation": "exp",
+    "sparse": sparse,
+    "seed": seed
+}
+
+prior_parameters = {
+    "observation_variance": (1., 10.),
+    "heterogeneities": heterogeneity,
+    "shrinkage_factor": (1., shrinkage),
+    "kernel_gp_factor_processes": (cor, 1., 1.),
+    "kernel_tgp_factor_processes": (cor, 0.5, 1.),
+    "kernel_gp_loading_processes": (cor, 0.1, 1.),
+    "kernel_tgp_loading_processes": (cor, 0.5, 1.),
+    "kernel_gp_factor": (cor, 1., 1.)
+}
+
+model = BFFModel(
+    sequences=eeg.sequence,
+    stimulus_order=eeg.stimulus_order,
+    target_stimulus=eeg.target,
+    **settings,
+    **prior_parameters
+)
+# -----------------------------------------------------------------------------
+
+
+
+# =============================================================================
+# INITIALIZE CHAIN
+torch.manual_seed(seed)
+status = False
+while not status:
+    try:
+        model.initialize_chain()
+        status = True
+    except Exception as e:
+        print(e)
+# -----------------------------------------------------------------------------
+
+
+
+# =============================================================================
+# RUN CHAIN
+torch.manual_seed(seed)
+t0 = time.time()
+t00 = t0
+for i in range(n_iter):
+    model.sample()
+    if i % 100 == 0:
+        print(f"{i:>10} "
+              f"{model.variables['observations']._log_density_history[-1]:>20.4f}"
+              f"  dt={time.time() - t00:>20.4f}   elapsed={time.time() - t0:20.4f}")
+        t00 = time.time()
+# -----------------------------------------------------------------------------
+
+
+
+# =============================================================================
+# SAVE CHAIN
+out = model.results(
+    start=10_000,
+    thin=10
+)
+with open(dir_chains + f"K{subject}_dim{K}_fold{fold}.chain", "wb") as f:
+    pickle.dump(out, f)
+# -----------------------------------------------------------------------------
+
