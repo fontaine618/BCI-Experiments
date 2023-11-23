c918b89e907ddf5a399caf218429ba9e5ed2f35c
fontaine618 <simfont@umich.edu>
2023-11-23 11:03:28 -0500

    ic jobs

--- latent_dimension/ic.sh
+++ latent_dimension/ic.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=bci_K
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=3:00:00
-#SBATCH --array=2-13
+#SBATCH --time=5:00:00
+#SBATCH --array=10-13
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
