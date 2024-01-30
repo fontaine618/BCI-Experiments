bbb7a5244c329421a1c5756667f33a46dd95a3f1
fontaine618 <simfont@umich.edu>
2024-01-30 00:21:23 -0500

    sim_variants rest

--- sim_variants/job.sh
+++ sim_variants/job.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=5:00:00
-#SBATCH --array=1-8
+#SBATCH --array=0-8
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
