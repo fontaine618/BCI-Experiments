139ce840e3ab3c4604cc6300cbd6352e7a259922
fontaine618 <simfont@umich.edu>
2024-02-23 00:12:18 -0500

    new subject select v

---
+++ subject/R_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=bci_subject_R
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=9:00:00
+#SBATCH --array=3-9
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=16g
+#SBATCH --output=/home/%u/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+module load python/3.10.4
+source /home/simfont/venvs/bci/bin/activate
+python -O R_train.py 114 $SLURM_ARRAY_TASK_ID
+python -O R_testmllk.py 114 $SLURM_ARRAY_TASK_ID
+
