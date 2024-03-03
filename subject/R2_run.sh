189c436259c611a371ba5174b2cdd395434da330
fontaine618 <simfont@umich.edu>
2024-03-02 23:29:38 -0500

    new subject R

---
+++ subject/R2_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#“#SBATCH” directives that convey submission options:
+#SBATCH --job-name=bci_subject_R2
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=simfont@umich.edu
+#SBATCH --time=10:00:00
+#SBATCH --array=2,3,4,5
+#SBATCH --account=stats_dept1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=16g
+#SBATCH --output=/home/%u/logs/%x-%j.log
+# The application(s) to execute along with its input arguments and options:
+module load python/3.10.4
+source /home/simfont/venvs/bci/bin/activate
+python -O R2_train.py 114 $SLURM_ARRAY_TASK_ID
+python -O R2_testmllk.py 114 $SLURM_ARRAY_TASK_ID
+python -O R2_test.py 114 $SLURM_ARRAY_TASK_ID
+
---
+++ subject/R2_testmllk.py
@@ -0,0 +1,111 @@
+import numpy as np
+import os
+import sys
+sys.path.insert(1, '/home/simfont/Documents/BCI/src')
+import torch
+import itertools as it
+from source.bffmbci import BFFMResults
+from source.data.k_protocol import KProtocol
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
+
+
+# =============================================================================
+# SETUP
+dir_results = "/home/simfont/Documents/BCI/experiments/subject/results/"
+dir_chains = "/home/simfont/Documents/BCI/experiments/subject/chains/"
+dir_data = "/home/simfont/Documents/BCI/K_Protocol/"
+os.makedirs(dir_results, exist_ok=True)
+
+
+# file
+type = "TRN"
+subject = str(sys.argv[1])
+session = "001"
+name = f"K{subject}_{session}_BCI_{type}"
+
+# preprocessing
+window = 800.0
+bandpass_window = (0.1, 15.0)
+bandpass_order = 2
+downsample = 8
+
+# model
+seed = 0
+K = 8
+V = ["LR-DCR", "LR-DC", "LR-SC"][0]
+cor = [0.35, 0.40, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8][3]
+n_iter = 20_000
+
+train_reps = 15
+every = int(sys.argv[2])
+
+# prediction settings
+factor_processes_method = "analytical"
+n_samples = 100
+
+# dimensions
+n_characters = 19
+n_repetitions = 15
+# -----------------------------------------------------------------------------
+
+
+# =============================================================================
+# LOAD DATA
+eeg = KProtocol(
+    filename=dir_data + name + ".mat",
+    type=type,
+    subject=subject,
+    session=session,
+    window=window,
+    bandpass_window=bandpass_window,
+    bandpass_order=bandpass_order,
+    downsample=downsample,
+)
+# subset training reps
+training_reps = list(range(1, train_reps+1, every))
+testing_reps = list(range(1, train_reps+1))
+testing_reps = [x for x in testing_reps if x not in training_reps]
+eeg = eeg.repetitions(testing_reps)
+
+nchars = eeg.stimulus_data["character"].nunique()
+nreps = eeg.stimulus_data["repetition"].nunique()
+order = eeg.stimulus_order
+sequence = eeg.sequence
+target = eeg.target
+character_idx = eeg.character_idx
+# -----------------------------------------------------------------------------
+
+
+
+# =============================================================================
+# LOAD RESULTS
+torch.cuda.empty_cache()
+results = BFFMResults.from_files(
+    [dir_chains + f"K{subject}_every{every}.chain"],
+    warmup=0,
+    thin=1
+)
+self = results.to_predict(n_samples=n_samples)
+# -----------------------------------------------------------------------------
+
+
+
+# =============================================================================
+# GET PREDICTIVE PROBABILITIES
+llk_long, chars = self.predict(
+    order=order,
+    sequence=sequence,
+    factor_samples=1,
+    character_idx=character_idx,
+    factor_processes_method=factor_processes_method,
+    drop_component=None,
+    batchsize=20
+)
+# save
+np.save(
+    dir_results + f"K{subject}_every{every}_testmllk.npy",
+    llk_long.cpu().numpy()
+)
+# -----------------------------------------------------------------------------
+
+
