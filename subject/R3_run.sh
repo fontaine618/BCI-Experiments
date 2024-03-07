23ab3cf4505b941f7a7df1b30d3695f9f36da596
fontaine618 <simfont@umich.edu>
2024-03-07 10:22:57 -0500

    new subject R3 frist 2

--- subject/R3_run.sh
+++ subject/R3_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=bci_subject_R3
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
-#SBATCH --time=00:10:00
+#SBATCH --time=10:00:00
 #SBATCH --array=0-1
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
