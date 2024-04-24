f43ead2d67bbe3b58bcdfc743f343e7bcc8199d9
fontaine618 <simfont@umich.edu>
2024-04-24 10:45:54 -0400

    run even/odd only

--- subject/R_run.sh
+++ subject/R_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=simfont@umich.edu
 #SBATCH --time=7:00:00
-#SBATCH --array=10-29
+#SBATCH --array=30
 #SBATCH --account=stats_dept1
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
